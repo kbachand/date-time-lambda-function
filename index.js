0bd8e661e67e9373dcc2dbf61129ca63ae325a12
kbachand <keithbachand14@gmail.com>
2018-03-26 16:58:42 -0400

    added comments to explain code

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+//AWS Lambda Function to write file with current date and time to S3 bucket
 exports.handler = (event, context, callback) => {
     callback(null, "successful");
+//AWS variable used to require aws-sdk to run this code
 var AWS = require('aws-sdk');
+//variable date to call later on to allow storing current and time
 var date = new Date();
+//s3bucket variable stores where to store file at (which specific Bucket)
 var s3bucket = new AWS.S3({params: {Bucket: 'health-finch-challenge/time-date'}});
+//function which will upload the file to the bucket
 function uploadFileOnS3(fileName, fileData){
     var params = {
       Key: fileName,
       Body: fileData, date
     };
-
+//calling function
     s3bucket.uploadFileOnS3(params, function (err, res) {
+      //Simple if else statement to log either and error or show function has ran successfully
         if(err)
             console.log("Error in uploading file on s3 due to "+ err);
         else
